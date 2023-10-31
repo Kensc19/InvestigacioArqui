6b35d652b002d07205dfe6829c8437528cf8408b
luisballar <luisballar123@gmail.com>
2023-10-30 22:03:13 -1100

    gui

--- src/main/java/com/example/investig_arqui/AddTasks.java
+++ src/main/java/com/example/investig_arqui/AddTasks.java
@@ -2,8 +2,13 @@ package com.example.investig_arqui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AddTasks {
 
@@ -23,12 +28,11 @@ public class AddTasks {
     private TextArea textArea_task;
 
     @FXML
-    void accept_but_clicked(ActionEvent event) {
-
+    void accept_but_clicked(ActionEvent event) throws IOException {
     }
 
     @FXML
-    void exit_add_clicked(ActionEvent event) {
+    void exit_add_clicked(ActionEvent event) throws IOException {
 
     }
 
--- src/main/java/com/example/investig_arqui/MainMenu.java
+++ src/main/java/com/example/investig_arqui/MainMenu.java
@@ -1,8 +1,15 @@
 package com.example.investig_arqui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenu {
 
@@ -15,23 +22,30 @@ public class MainMenu {
     @FXML
     private Button showButton;
 
-    @FXML
-    void addButton_clicked(ActionEvent event) {
 
+    @FXML
+    void addButton_clicked(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("addTasks.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage nuevoStage = new Stage();
+        nuevoStage.setScene(scene);
+        nuevoStage.show();
     }
+
+
+    @FXML
+    void showButton_clicked(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("showTasks.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage nuevoStage = new Stage();
+        nuevoStage.setScene(scene);
+        nuevoStage.show();
+    }
+
 
     @FXML
     void exitButtonAll_clicked(ActionEvent event) {
-
+        Platform.exit();
     }
-
-    @FXML
-    void showButton_clicked(ActionEvent event) {
-
-    }
-
-
-
-
 
 }
